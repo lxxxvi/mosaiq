eb67716d3358150628dbbd0257edcd7536bcbb4e
lxxxvi <lxxxvi@users.noreply.github.com>
2019-11-01 17:27:05 +0100

    remove unused code

--- test/svg_test.rb
+++ test/svg_test.rb
@@ -14,23 +14,4 @@ class SvgTest < Minitest::Test
     svg = Mosaiq::Image.new(width: 1, height: 1, palette: ['#fff']).svg
     assert_equal read_fixture('1x1_fff_base64.txt').chop, svg.to_base64
   end
-
-  private
-
-  def svg_element(nhtml)
-    nhtml.css('svg').first
-  end
-
-  def rect_elements(nhtml)
-    nhtml.css('rect')
-  end
-
-  def assert_attributes(args = {})
-    element = args.delete(:element)
-
-    args.each do |key, value|
-      assert_equal value, element.attr(key),
-                   "Expected element '#{element}' to have attribute '#{key}' with value '#{value}'"
-    end
-  end
 end
